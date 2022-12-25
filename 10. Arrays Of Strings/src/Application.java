fe950f72696c0cfc3a16f03ef1155ef2ce6e8b67
shinjuno123 <shinjuno123@naver.com>
2022-12-25 23:27:59 +0900

    Create more tutorials

---
+++ 10. Arrays Of Strings/src/Application.java
@@ -0,0 +1,32 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		String[] words =  new String[3];
+		
+		
+		words[0] = "Hello";
+		words[1] = "to";
+		words[2] = "you";
+		
+		System.out.println(words[2]);
+		
+		String[] fruits = {"apple", "banana", "pear", "kiwi"};
+		
+		for(String fruit: fruits) {
+			System.out.println(fruit);
+		}
+		
+		int vaalue = 0;
+		
+		String text = null; // null means it is not pointing anywhere
+		
+		System.out.println(text);
+		
+		String[] texts = new String[2];
+		System.out.println(texts[0]);
+		
+		texts[0] = "one";
+	}
+
+}
--- 9. Arrays/src/Application.java
+++ 9. Arrays/src/Application.java
@@ -1,8 +1,32 @@
+import java.util.ArrayList;
 
 public class Application {
 
 	public static void main(String[] args) {
+		int value = 7;
 
+		int[] values; // reference type
+		values = new int[3];
+
+		System.out.println(values[0]);
+
+		values[0] = 10;
+		values[1] = 40;
+		values[2] = 20;
+
+		System.out.println(values[0]);
+		System.out.println(values[1]);
+		System.out.println(values[2]);
+
+		for (int i = 0; i < values.length; i++) {
+			System.out.println(values[i]);
+		}
+		
+		int[] numbers = {5, 6, 7};
+		
+		for (int i=0; i < numbers.length;i++) {
+			System.out.println(numbers[i]);
+		}
 	}
 
 }
